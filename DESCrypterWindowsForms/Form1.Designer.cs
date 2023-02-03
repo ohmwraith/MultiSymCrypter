6358f0f7eeb5203b55dfcddac738d43c2257bc49
ohmwraith <vaselite10@yandex.ru>
2023-02-03 03:52:38 +0300

    Первый коммит

---
+++ DESCrypterWindowsForms/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DESCrypterWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStripCrypt = new System.Windows.Forms.MenuStrip();
+            this.шифрованиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьКлючToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьКлючToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.зашифроватьИнформациюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отобразитьШифрованнуюИнформациюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дешифрованиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripCrypt.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(46, 305);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(245, 305);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // menuStripCrypt
+            // 
+            this.menuStripCrypt.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStripCrypt.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.шифрованиеToolStripMenuItem,
+            this.дешифрованиеToolStripMenuItem});
+            this.menuStripCrypt.Location = new System.Drawing.Point(0, 0);
+            this.menuStripCrypt.Name = "menuStripCrypt";
+            this.menuStripCrypt.Size = new System.Drawing.Size(800, 29);
+            this.menuStripCrypt.TabIndex = 2;
+            this.menuStripCrypt.Text = "Шифрование";
+            // 
+            // шифрованиеToolStripMenuItem
+            // 
+            this.шифрованиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьКлючToolStripMenuItem,
+            this.загрузитьКлючToolStripMenuItem,
+            this.зашифроватьИнформациюToolStripMenuItem,
+            this.отобразитьШифрованнуюИнформациюToolStripMenuItem});
+            this.шифрованиеToolStripMenuItem.Name = "шифрованиеToolStripMenuItem";
+            this.шифрованиеToolStripMenuItem.Size = new System.Drawing.Size(119, 25);
+            this.шифрованиеToolStripMenuItem.Text = "Шифрование";
+            // 
+            // создатьКлючToolStripMenuItem
+            // 
+            this.создатьКлючToolStripMenuItem.Name = "создатьКлючToolStripMenuItem";
+            this.создатьКлючToolStripMenuItem.Size = new System.Drawing.Size(391, 26);
+            this.создатьКлючToolStripMenuItem.Text = "Создать ключ";
+            this.создатьКлючToolStripMenuItem.Click += new System.EventHandler(this.создатьКлючToolStripMenuItem_Click);
+            // 
+            // загрузитьКлючToolStripMenuItem
+            // 
+            this.загрузитьКлючToolStripMenuItem.Name = "загрузитьКлючToolStripMenuItem";
+            this.загрузитьКлючToolStripMenuItem.Size = new System.Drawing.Size(391, 26);
+            this.загрузитьКлючToolStripMenuItem.Text = "Загрузить ключ";
+            this.загрузитьКлючToolStripMenuItem.Click += new System.EventHandler(this.загрузитьКлючToolStripMenuItem_Click);
+            // 
+            // зашифроватьИнформациюToolStripMenuItem
+            // 
+            this.зашифроватьИнформациюToolStripMenuItem.Name = "зашифроватьИнформациюToolStripMenuItem";
+            this.зашифроватьИнформациюToolStripMenuItem.Size = new System.Drawing.Size(391, 26);
+            this.зашифроватьИнформациюToolStripMenuItem.Text = "Зашифровать информацию";
+            this.зашифроватьИнформациюToolStripMenuItem.Click += new System.EventHandler(this.зашифроватьИнформациюToolStripMenuItem_Click);
+            // 
+            // отобразитьШифрованнуюИнформациюToolStripMenuItem
+            // 
+            this.отобразитьШифрованнуюИнформациюToolStripMenuItem.Name = "отобразитьШифрованнуюИнформациюToolStripMenuItem";
+            this.отобразитьШифрованнуюИнформациюToolStripMenuItem.Size = new System.Drawing.Size(391, 26);
+            this.отобразитьШифрованнуюИнформациюToolStripMenuItem.Text = "Отобразить шифрованную информацию";
+            // 
+            // дешифрованиеToolStripMenuItem
+            // 
+            this.дешифрованиеToolStripMenuItem.Name = "дешифрованиеToolStripMenuItem";
+            this.дешифрованиеToolStripMenuItem.Size = new System.Drawing.Size(136, 25);
+            this.дешифрованиеToolStripMenuItem.Text = "Дешифрование";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStripCrypt);
+            this.MainMenuStrip = this.menuStripCrypt;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStripCrypt.ResumeLayout(false);
+            this.menuStripCrypt.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.MenuStrip menuStripCrypt;
+        private System.Windows.Forms.ToolStripMenuItem шифрованиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьКлючToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem зашифроватьИнформациюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отобразитьШифрованнуюИнформациюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дешифрованиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьКлючToolStripMenuItem;
+    }
+}
+
---
+++ DESCrypterWindowsForms/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DESCrypterWindowsForms
+{   
+    public class KeyFileHandler
+    {
+        public static byte[] load(string filename)
+        {
+            byte[] key = new byte[8];
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            fs.Read(key, 0, key.Length);
+            fs.Close();
+            return key;
+        }
+        public static void save(byte[] key, string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.OpenOrCreate);
+            fs.Write(key, 0, key.Length);
+            fs.Close();
+        }
+    }
+    public class TextFileHandler
+    {
+        public static string load(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string text = sr.ReadToEnd();
+            fs.Close();
+            sr.Close();
+            return text;
+        }
+        public static void save(string text, string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(text);
+            fs.Close();
+            sw.Close();
+        }
+    }
+    public partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+        String stored_text;
+
+        private void создатьКлючToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                KeyFileHandler.save(DES.Key, sfd.FileName);
+                MessageBox.Show("Ключ успешно сохранен", "Создание ключа", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void загрузитьКлючToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                DES.Key = KeyFileHandler.load(ofd.FileName);
+                MessageBox.Show("Ключ успешно загружен", "Загрузка ключа", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void зашифроватьИнформациюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK) stored_text = TextFileHandler.load(ofd.FileName);
+
+            ICryptoTransform transform = DES.CreateEncryptor();
+            String crypted_filename = Path.ChangeExtension(ofd.FileName, "crypt");
+            using (FileStream fileStreamEncrypt = new FileStream(crypted_filename, FileMode.OpenOrCreate))
+            {
+                // Запись случайного вектора инициализации без его шифрования
+                fileStreamEncrypt.Write(DES.IV, 0, DES.IV.Length);
+                // Создание криптографического потока в режиме записи
+                using (CryptoStream cryptoStream = new CryptoStream(fileStreamEncrypt, transform, CryptoStreamMode.Write))
+                {
+                    // Создание объекта записи текста, который будет преобразовывать текст в
+                    // двоичные данные
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        // Запись зашифрованной информации и очистка буфера памяти
+                        streamWriter.Write(stored_text);
+                        streamWriter.Flush();
+                        
+                        // В конце операции шифрования необходимо дополнить заключительный частичный
+                        // блок нулями и записать его в файл
+                        cryptoStream.FlushFinalBlock();
+                    }
+                }
+            }
+            // Открытие файла для чтения
+            using (FileStream fileStreamDecrypt = new FileStream(crypted_filename, FileMode.Open))
+            {
+                // Пропуск вектора инициализации
+                fileStreamDecrypt.Seek(DES.IV.Length, SeekOrigin.Begin);
+                // Чтение зашифрованного текста
+                using (StreamReader streamReader = new StreamReader(fileStreamDecrypt))
+                {
+                    richTextBox1.Text = streamReader.ReadToEnd();
+                }
+
+                MessageBox.Show("Шифртекст успешно сохранен!", "Шифрование", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+    }
+}
